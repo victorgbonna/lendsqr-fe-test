9291f161782ec2015d6ba40285f8a6e1d45c3faf
GreyHaired <victorgbonna@gmail.com>
2022-10-17 13:53:04 +0100

    converted css to scss, tried my best in typescript, finished the filter button UI and functionality, logo click should redirect you to the home page,  good to go

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import './Logo.scss'
 
 export default function Logo() {
     return (
-    <div className='logo'>
+    <Link to='/' className='logo'>
         <img src="/svg/union.svg" alt="Lendr logo" />
         <h3>Lendsqr</h3>
-    </div>
+    </Link>
     );
 }
   
